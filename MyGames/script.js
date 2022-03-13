cb5896d46ee6745135ec875eb208abd48dd61c31
Kevin "d415uk3" Wallen <46876927+daisukedd@users.noreply.github.com>
2022-03-13 13:43:29 -0300

    Projeto - Aluraflix
    Chamei de MyGames e optei por colocar os jogos que estou jogando no momento.

---
+++ MyGames/script.js
@@ -0,0 +1,5 @@
+var myGames = ["https://images.tcdn.com.br/img/img_prod/649042/pokemon_shield_nintendo_switch_3439_1_956b85dc4e74067d46aaf50417ef6656.jpg","https://m.media-amazon.com/images/I/81qKst2CMOL._AC_SL1500_.jpg","https://m.media-amazon.com/images/I/71pNu+RikwL._AC_SY445_.jpg"]
+
+for (var i = 0; i < myGames.length; i++) {
+    document.write("<img src=" + myGames[i] + ">")
+}
